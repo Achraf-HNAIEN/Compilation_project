3c00b805611f8af011e39b814c5dd74a095e3993
Achraf-HNAIEN <achraf.hnaien62@gmail.com>
2024-04-02 17:23:58 +0200

    Refactor symbol.h file

--- src/symbole.h
+++ src/symbole.h
@@ -3,36 +3,33 @@
 
 #include <stdbool.h>
 
-// Définition des types de symboles
+// Definition of symbol types
 typedef enum { VARIABLE, FUNCTION, PARAMETER } SymbolType;
 
 typedef enum { INT, CHAR } DataType;
 
-// Structure pour un symbole
+// Structure for a symbol
 typedef struct Symbol {
-  char* name;         // Nom
-  SymbolType type;    // Type de symbole (variable, fonction, paramètre)
-  DataType dataType;  // Type de donnée (int, char)
-  // bool isGlobal;            // Indique si le symbole est global 0 si oui le
-  // reste non
-  int address;     // Adresse dans la mémoire
-  int scopeLevel;  // Niveau de portée du symbole
-  int lineNumber;  // Ligne dans le code source
+  char* name;         // Name
+  SymbolType type;    // Symbol type (variable, function, parameter)
+  DataType dataType;  // Data type (int, char)
+  int address;        // Address in memory
+  int scopeLevel;     // Symbol scope level
+  int lineNumber;     // Line number in source code
   struct Symbol* next;
 } Symbol;
 
 typedef struct {
-  Symbol* head;  // Pointeur vers le premier symbole de la liste
-  int size;      // Nombre de symboles dans la table
+  Symbol* head;  // Pointer to the first symbol in the list
+  int size;      // Number of symbols in the table
 } SymbolTable;
 
-// Fonctions pour la gestion de la table des symboles
+// Functions for symbol table management
 void initSymbolTable(SymbolTable* table);
 Symbol* addSymbol(SymbolTable* table,
                   const char* name,
                   SymbolType type,
                   int dataType,
-                  // bool isGlobal,
                   int lineNumber);
 Symbol* findSymbol(SymbolTable* table, const char* name);
 void deleteSymbolTable(SymbolTable* table);
