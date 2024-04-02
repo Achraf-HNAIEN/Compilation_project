4f9f112e7e93b0ff3bad73d5bbf94ad693a1d87b
Achraf-HNAIEN <achraf.hnaien62@gmail.com>
2024-04-02 17:23:58 +0200

    Add symbol creation and deletion functions to SymbolTable

--- src/symbole.c
+++ src/symbole.c
@@ -13,8 +13,45 @@ Symbol* addSymbol(SymbolTable* table,
                   const char* name,
                   SymbolType type,
                   int dataType,
-                  int lineNumber) {}
+                  int lineNumber) {
+  Symbol* newSymbol = (Symbol*)malloc(sizeof(Symbol));
+  if (!newSymbol)
+    return NULL;
+  newSymbol->name = strdup(name);
+  newSymbol->type = type;
+  newSymbol->dataType = dataType;
+  newSymbol->lineNumber = lineNumber;
+  newSymbol->next = NULL;
+  if (!table->head) {
+    table->head = newSymbol;
+  } else {
+    newSymbol->next = table->head;
+    table->head = newSymbol;
+  }
+  table->size++;
+  return newSymbol;
+}
 
-Symbol* findSymbol(SymbolTable* table, const char* name) {}
+Symbol* findSymbol(SymbolTable* table, const char* name) {
+  Symbol* current = table->head;
+  while (current) {
+    if (strcmp(current->name, name) == 0) {
+      return current;
+    }
+    current = current->next;
+  }
+  return NULL;
+}
 
-void deleteSymbolTable(SymbolTable* table) {}
+void deleteSymbolTable(SymbolTable* table) {
+  Symbol* current = table->head;
+  Symbol* next;
+  while (current) {
+    next = current->next;
+    free(current->name);
+    free(current);
+    current = next;
+  }
+  table->head = NULL;
+  table->size = 0;
+}
